a6692157067b7f2d44fd5cbda863dd882abf6c0d
Leonardo Silva <leonardo.oliveirasilva1005@gmail.com>
2026-03-12 22:57:57 -0300

    Setup lint

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**']
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        parser: tseslint.parser
+      }
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    }
+  }
+)
